fcca7daf673117e2afdd13caf7e9962eb8579624
MansiSri <37022283+mansi2921@users.noreply.github.com>
2020-06-25 19:30:05 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,3 +1,4 @@
 echo $GITHUB_SHA
 curl "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA" | grep -F filename | awk '{print$2}' | cut -d \, -f1 > uploadedfile.log
 git show $GITHUB_SHA --name-only
+env
